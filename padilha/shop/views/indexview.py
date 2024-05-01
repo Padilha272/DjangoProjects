95d5e6cb9f3497a6c20af0362e188c5e5da5216f
Matheus de Araújo Padilha <mapadilha02@gmail.com>
2024-05-01 20:09:39 -0300

    Search Bar implementation - 01/05/2024

--- padilha/shop/views/indexview.py
+++ padilha/shop/views/indexview.py
@@ -11,13 +11,18 @@ class IndexView(TemplateView):
     
     def get(self, request, id=None):
         categories = Category.objects.order_by("name")
-        if id is None: 
+        filter = request.GET.get("q","") # get q or empty string
+        if filter !="":
+            title = f"search results for '{filter}'"
+            products = Product.objects.filter(name__icontains=filter).order_by("name")
+        elif id is None: 
             title = "All products"          
             products = Product.objects.order_by("name")
         else:
             title = next(c.name for c in categories if c.id ==id)
             products = Product.objects.filter(category_id=id).order_by("name")
         context = {
+            "filter": filter,
             "categories": [{"id":c.id,"name":c.name} for c in categories],
             "title": title,
             "products":[{"id" : p.id,"name":p.name, "price":p.price ,"image": p.image} for p in products]}
