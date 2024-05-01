315f73433907d27248cea1ee661a850605d1871f
Matheus de Araújo Padilha <mapadilha02@gmail.com>
2024-05-01 18:18:32 -0300

    Categories-01/05/2024

--- padilha/shop/models.py
+++ padilha/shop/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return f"{self.name} ({self.id})"
+
 class Product(models.Model):
     name = models.CharField(max_length=38)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.FloatField()
     volume = models.CharField(max_length=10)
     description = models.CharField(max_length=255)
